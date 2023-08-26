964d47649045cb52309c19d4a617cdca8794beef
ghy99123 <2941879036@qq.com>
2023-08-26 16:20:29 +1000

    fix: delete audio only setting

--- src/pages/social/AppBar/DropdownMenu.tsx
+++ src/pages/social/AppBar/DropdownMenu.tsx
@@ -4,14 +4,8 @@ import MenuItem from "@mui/material/MenuItem";
 import { IconButton } from "@mui/material";
 import { MoreVert } from "@mui/icons-material";
 import { logout } from "../../../utils/auth";
-import { useAppSelector } from "../../../redux/hooks";
-import { setAudioOnly } from "../../../redux/reducers/room/roomSlice";
-import { useDispatch } from "react-redux";
 
 export default function BasicMenu() {
-  const dispatch = useDispatch();
-  const { audioOnly } = useAppSelector((state) => state.room);
-
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -19,10 +13,6 @@ export default function BasicMenu() {
   };
   const handleClose = () => {
     setAnchorEl(null);
-  };
-
-  const handleAudioOnlyClick = () => {
-    dispatch(setAudioOnly(!audioOnly));
   };
 
   return (
@@ -41,9 +31,6 @@ export default function BasicMenu() {
       >
         <MenuItem onClick={logout} style={{ color: "black" }}>
           Logout
-        </MenuItem>
-        <MenuItem onClick={handleAudioOnlyClick} style={{ color: "black" }}>
-          {audioOnly ? "Audio Only Enabled" : "Audio Only Disabled"}
         </MenuItem>
       </Menu>
     </div>
